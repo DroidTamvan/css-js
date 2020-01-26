9e2addb6994c2347cb190b007ba83b1142de94fd
DroidTamvan <32235526+DroidTamvan@users.noreply.github.com>
2020-01-27 04:37:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('http://droidtamvan.gitlab.io/ServiceWorker.js').then(function(registration) {
+    navigator.serviceWorker.register('https://droidtamvan.gitlab.io/ServiceWorker.js').then(function(registration) {
       console.log('ServiceWorker registration successful with scope');
     }, function(err) {
       console.log('ServiceWorker registration failed: ', err);
